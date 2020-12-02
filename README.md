88fb0e72b3c9f90fba3dbc3cce57c4ea384425b9
Liksu <github@Liksu.kiev.ua>
2020-12-03 01:20:12 +0200

    Code and documentation

--- README.md
+++ README.md
@@ -1,1 +1,276 @@
 # svg-easy-tree
+
+Compiles simple tree object into DOM structure (SVG or HTML) and add it to a parent element.
+
+Tree is a structure of `node` objects that describes DOM that you want to get.
+
+## Node
+
+Each `node` is an object that represents one DOM element.
+
+Allowed properties (jsdoc notation):
+
+```
+ @property {String}         tag
+ @property {String|Number} [_id]
+ @property {Object}        [opt]
+ @property {String|Number} [val]
+ @property {Boolean}       [sav]
+ @property {Array<node>}   [sub]
+ @property {Element}       [_el]
+```
+
+#### Property `tag`
+
+_Type: String_
+
+Element tag name. For example `'svg'`.
+
+It will produce element `<svg></svg>`.
+
+Also, `tag` can contain an `Element` instance, and it will be updated with other passed settings.
+
+#### Property `opt`
+
+_Type: Object_
+
+Plain object that contains all element attributes.
+
+Example:
+
+```json
+{
+  "tag": "svg",
+  "opt": {
+           "width": 400,
+           "height": 400,
+           "viewBox": "0 0 400 400"
+         }
+}
+```
+
+will produce
+
+```html
+<svg width="400" height="400" viewBox="0 0 400 400"></svg>
+```
+
+#### Property `val`
+
+_Type: String | Number_
+
+Sets the inner value of created element. By default, that means some text or number,
+but in real the passed value will be put in the element.innerHTML,
+so it can be anything, including other elements markup.
+
+Example:
+
+```json
+{
+  "tag": "text",
+  "val": "42 is the Answer"
+}
+```
+
+Result:
+
+```html
+<text>42 is the Answer</text>
+```
+
+#### Property `sub`
+
+_Type: Array<node>_
+
+Contains subtree, array of nodes that will be compiled recursively.
+Can also contains everything, that can be appended to DOM element.
+
+This property will be processed after `val` property, so subtree will be appended after element's value.
+
+Example:
+
+```js
+tspan = document.createElement('tspan');
+tspan.innerHTML = '42';
+
+new Tree().compile({
+  "tag": "svg",
+  "sub": [
+    {tag: 'text', sub: [tspan]},
+    {tag: 'line', opt: {x1: 0, y1: 0, x2: 100, y2: 100}}
+  ]
+})
+```
+
+Result (pretty printed):
+
+```html
+<svg>
+    <text>
+        <tspan>42</tspan>
+    </text>
+    <line x1="0" y1="0" x2="100" y2="100"></line>
+</svg>
+```
+
+#### Property `_id`
+
+_Type: String_
+
+Sets `x-id` attribute, that allows finding node into the DOM or in the tree.
+
+Not replace `id` attribute, so in the DOM, you still can set `id` with `opt`
+and then get a generated element using `getElementById`.
+
+Example:
+
+```json
+{
+  "tag": "text",
+  "opt": {
+    "id": "text-element",
+    "data": {
+      "innerId": "dataId"
+    }
+  },
+  "_id": "text-node"
+}
+```
+
+Result:
+
+```html
+<text id="text-element" data-inner-id="dataId" x-id="text-node"></text>
+```
+
+#### Properties `sav` and `_el`
+
+_Type `sav`: Boolean_
+
+_Type `_el`: SVGElement | HTMLElement_
+
+Two these properties are directly tied, because `_el` property
+will automatically contains link to generated DOM element, if `sav` property sets to `true`.
+
+Example:
+
+```js
+const treeObject = {
+  "tag": "text",
+  "_id": "text-node",
+  "sav": true
+};
+
+new Tree().compile(treeObject)
+```
+
+Result:
+
+content of `treeObject`:
+
+```json
+{
+    "tag": "text",
+    "_id": "text-node",
+    "sav": true,
+    "opt": {"x-id": "text-node"},
+    "_el": text
+}
+```
+
+where `text` is a link to generated `SVGElement`, and `treeObject._el.outerHTML` returns:
+
+```html
+<text x-id="text-node"></text>
+```
+
+## Tree instance properties
+
+### tree
+
+Contains tree structure to work with.
+
+### xmlns
+
+Represents namespace for created elements.
+
+## Methods
+
+### constructor
+
+Creates an instance of `Tree`.
+
+Parameters:
+
+* `initialTree` - optional, `node` that contains tree to start working with, default `{}`
+* `xmlns` - optional, namespace that will be used for creating elements. By default will be used SVG namespace.
+
+### compile
+
+Compiles a passed `node` to Element (HTML or SVG, depends on `xmlns` parameter in the constructor),
+and append it to parent, if passed.
+
+Parameters:
+
+* `node` - optional, node to process, `this.tree` by default,
+means tree, that was passed as `initialTree` to constructor and stored to further work.
+* `parent` - optional, DOM Element to append compiled element
+
+Returns created element.
+
+### recompile
+
+Compile a new node and replace an old element with it.
+
+Requires to be enabled `sav` into a previously compiled node (at least it should exist `_el` link to existed element in the DOM).
+
+It is possible to update `node` into the tree and run `recompile`, without passing any parameters,
+in this case whole tree will be recompiled and updated into the DOM.
+
+Parameters:
+
+* `oldNode` - optional, node to be removed, `this.tree` by default
+* `newNode` - optional, node to be compiled and placed instead of old node, `oldNode` by default
+
+### find
+
+Find matches node from the tree.
+
+Needs some rework, so now it is better to use as search by `_id`.
+
+Parameters:
+
+* `selector` - string describes search conditions (`_id` to find)
+* `tree` - optional, node to start recursive search, `this.tree` by default
+
+Returns found node or `null`.
+
+### append
+
+Appends passed subtree (node) to parent `sub`.
+
+Parameters:
+
+* `node` - parent node, that will contains subtree
+* `subTree` - node to append
+* `before` - optional, boolean flag that change append method to prepend, `false` by default
+
+Returns `node`.
+
+There are two options to use: `append(node, subTree)` and `append(subTree)`.
+
+In the second variant, `subTree` will be added to `this.tree`.
+
+### prepend
+
+Same as `append`, but set `before` flag to `true`.
+
+### analyze
+
+Restore tree from existing DOM element.
+
+Parameters:
+
+* `element` - DOM element to analyze
+
+Returns `node` with serialized tree.
---
+++ tree.js
@@ -0,0 +1,224 @@
+/**
+ * (c) Petro Borshchahivskyi
+ * https://github.com/Liksu/svg-easy-tree
+ */
+
+/**
+ * @typedef  {Object}    node
+ * @property {String}         tag
+ * @property {String|Number} [_id]
+ * @property {Object}        [opt]
+ * @property {String|Number} [val]
+ * @property {Boolean}       [sav]
+ * @property {Array<node>}   [sub]
+ * @property {Element}       [_el]
+ */
+
+export default class Tree {
+    static ns_HTML = 'http://www.w3.org/1999/xhtml';
+    static ns_SVG = 'http://www.w3.org/2000/svg';
+    static attributeId = 'x-id';
+
+    constructor(initialTree, xmlns = Tree.ns_SVG) {
+        this.xmlns = xmlns;
+        this.tree = initialTree instanceof Element
+            ? this.analyze(initialTree)
+            : initialTree || {};
+    }
+
+    /**
+     * Create dom element from node object
+     *
+     * @param {node} node
+     * @param {Element} [parent]
+     * @returns {Element}
+     */
+    createNode(node, parent) {
+        const el = node.tag instanceof Element ? node.tag : document.createElementNS(this.xmlns, node.tag);
+        if (!node.tag) return null;
+
+        if (node.val != null) {
+            el.innerHTML = node.val;
+        }
+        if (node.opt) {
+            Object.entries(node.opt).forEach(([key, value]) => {
+                if (key === 'data' && typeof value === 'object') {
+                    Object.entries(value).forEach(([dataKey, dataValue]) => {
+                        el.dataset[dataKey] = dataValue;
+                    });
+                } else {
+                    el.setAttributeNS(null, key, value);
+                }
+            });
+        }
+        if (parent) parent.appendChild(el);
+        return el;
+    }
+
+    /**
+     * Compile tree into dom structure and append it to parent element
+     *
+     * @param {node} node
+     * @param {Element} parent
+     * @returns {null|Element}
+     */
+    compile(node = this.tree, parent = null) {
+        if (!node) node = this.tree;
+        if (!node.tag) return null;
+
+        if (node._id) {
+            if (!node.opt) node.opt = {};
+            node.opt[Tree.attributeId] = node._id;
+        }
+
+        const el = this.createNode(node, parent);
+        if (node.sav) node._el = el;
+        if (node.sub) node.sub.forEach(sub => {
+            if (sub instanceof Element) el.appendChild(sub);
+            else sub && this.compile(sub, el);
+        });
+
+        return el;
+    }
+
+    /**
+     * Compile new node and replace old element with it
+     *
+     * @param {node} [oldNode]
+     * @param {node} [newNode]
+     */
+    recompile(oldNode = this.tree, newNode = oldNode) {
+        if (!oldNode._el) {
+            console.error('Trying to recompile node without stored element');
+            return;
+        }
+
+        const oldElement = oldNode._el;
+        const newElement = this.compile(newNode);
+        return oldElement.parentElement.replaceChild(newElement, oldElement);
+    }
+
+    /**
+     * works not really correct, need rework
+     * has issue with returning found value from deep like '/main/hours/line', will return hours node instead of line
+     * has issue with partial selectors like 'hours/line' instead of full '/main/hours/line'
+     * best usage for now is to find plugin by id
+     *
+     * @param selector - id of node or path from root like '/main/...', if node has _id it will be used to compare with selector, otherwise - tag
+     * @param [tree]
+     * @param [path]
+     * @returns {null|{}}
+     */
+    find(selector, tree = this.tree, path = '') {
+        if (tree._id == selector) return tree;
+        const treePath = path + '/' + (tree._id || tree.tag || '');
+        if (treePath === selector) return tree;
+        if (tree.sub) {
+            return tree.sub.filter(subTree => typeof subTree === 'object')
+                .find(subTree => this.find(selector, subTree, treePath)) || null;
+        }
+        return null;
+    }
+
+    /**
+     * init code, not finished
+     * should return tree node by xpath
+     *
+     * @param {String|Number} selector
+     * @param {node} tree
+     * @returns {*[]|*|{}}
+     */
+    q(selector, tree = this.tree) {
+        if (tree._id == selector) return tree;
+        if (this._cache[selector]) return this._cache[selector][0];
+        const path = String(selector).split('/');
+        const catches = path.map(value => this._cache[value]);
+        //catches.reduce(() => {}, );
+        return catches;
+    }
+
+    /**
+     * Appends subTree to passed node
+     *
+     * @param {node|string} node - node to append subTree or selector to find such node
+     * @param {node|Object|null|Array<node>} subTree
+     * @param {boolean} [before=false] - prepend if true
+     * @returns {node}
+     */
+    append(node, subTree, before = false) {
+        // selector passed
+        if (node && subTree && String(node) === node) {
+            node = this.find(node);
+        }
+
+        // node not passed
+        if (node && subTree == null && node instanceof Object) {
+            subTree = node;
+            node = this.tree;
+        }
+
+        if (node) {
+            if (!node.sub) node.sub = [];
+            if (subTree instanceof Tree) subTree = subTree.tree;
+            if (!(subTree instanceof Array)) subTree = [subTree];
+            const method = before ? 'unshift' : 'push';
+            node.sub[method](...subTree);
+        }
+
+        return node;
+    }
+
+    /**
+     * Prepend subTree to node
+     *
+     * @param {node|string} node - node to append subTree or selector to find such node
+     * @param {node|Object|null|Array<node>} subTree
+     * @returns {node}
+     */
+    prepend(node, subTree) {
+        return this.append(node, subTree, true);
+    }
+
+    /**
+     * Restore tree from existing DOM
+     *
+     * @param _el - DOM element
+     * @returns {node}
+     */
+    analyze(_el) {
+        const opt = Object.fromEntries(Array.from(_el.attributes, a => [a.name, restoreTypes(a.value)]));
+        const node = {
+            _el,
+            opt,
+            tag: _el.tagName,
+            sav: true,
+            sub: []
+        };
+
+        if (_el.children.length) {
+            for (let child of _el.children) {
+                node.sub.push(this.analyze(child));
+            }
+        }
+
+        if (opt[Tree.attributeId]) {
+            node._id = node.opt[Tree.attributeId];
+            delete node.opt[Tree.attributeId];
+        }
+
+        const val = Array.from(_el.childNodes)
+            .filter(n => n.nodeType === Text.TEXT_NODE)
+            .map(n => n.nodeValue);
+
+        if (val.length) {
+            node.val = restoreTypes(val.join('\n'));
+        }
+
+        return node;
+    }
+}
+
+function restoreTypes(string) {
+    if (!isNaN(parseFloat(string)) && isFinite(string)) return Number(string);
+    return string;
+}
